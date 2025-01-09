6a9a950dd8b34390caf496fc97812a43c246a618
osharpsand <osharpsand@gmail.com>
2025-01-08 21:19:44 -0700

    add functions to Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -1,6 +1,56 @@
 using System;
+using System.Collections.Generic;
 
-public static class Uitls 
+public static class Utils 
 {
-  
+  public bool CanPlayerTakeTokens(Dictionary<Currency, int> tokensWanted)
+  {
+    //Setup
+    int amountOfTokensWanted = 0;
+    int tokenTypesAvaliable = 0;
+
+    foreach (KeyValuePair<Currency, int> tokensOfTypeWanted in tokensWanted)
+    {
+      Currency tokenWanted = tokensOfTypeWanted.key;
+      int amountWanted = tokensOfTypeWanted.value;
+
+      if (GameManager.tokenPiles[tokenWanted] < amountWanted)
+      {
+        return false;
+      } 
+      else
+      {
+        amountOfTokensWanted += amountWanted;
+      }
+
+    }
+
+    foreach (KeyValuePair<Currency, int> tokensAvaliable in GameManager.tokenPiles)
+    {
+      if (tokensAvaliable.value > 0)
+      {
+        tokenTypesAvaliable++;
+      }
+    }
+
+    if (amountOfTokensWanted == 2) 
+    {
+      //If there is only one type of token they are trying to take, they can grab two of it.
+      return (tokensWanted.Count == 1);
+    } 
+    else if (amountOfTokensWanted == 3)
+    {
+      int differentTokens = tokensWanted.Count;
+      //Check if there is 3 different types of tokens. If true then they can take it.
+      if (differentTokensWanted == 3) { return true; }
+
+      //Players can grab multiple of one color as long as there isn't enough different token colors for them to pick from. They can only grab all 3 of one color if it is the only color avaliable.
+      if (differentTokensWanted == 2 && tokenTypesAvaliable == 2) { return true; }
+      if (differentTokensWanted == 1 && tokenTypesAvaliable == 1) { return true; }
+
+      //Return false if it was invalid.
+    }
+
+    return false;
+  }
 }
